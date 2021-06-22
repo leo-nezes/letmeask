bcfbaa8ca160cf80a93964b593e7bc84b7616904
Leonardo Menezes <leonhard.menezes@gmail.com>
2021-06-22 15:05:25 -0300

    feat(navigation): Navigation between pages - Adding new dependency - Adding routes to App - Adding navigation between homepage and new room page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 import './styles/global.scss';
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" exact component={NewRoom}/>
+    </BrowserRouter>
   );
 }
 
