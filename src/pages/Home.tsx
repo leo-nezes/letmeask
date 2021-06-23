0103b4077f49eddcc18a00ae788cb44d592c35f9
Leonardo Menezes <leonhard.menezes@gmail.com>
2021-06-23 16:30:34 -0300

    feat(Room page): Creating new Room page - Adding new routes to app - Creating function to join room - Creating function to create new room - Creating and styling new page Room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,18 @@
+import { FormEvent, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
+import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
-import { Button } from '../components/Button';
 
 export function Home() {
+  const [roomCode, setRoomCode] = useState('');
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
 
@@ -17,6 +20,22 @@ export function Home() {
     if(!user) await signInWithGoogle();
 
     history.push('/rooms/new');
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if(roomCode.trim() === '')
+      return;
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if(!roomRef.exists()) {
+      alert('Room does not exists.')
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -34,10 +53,12 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"  
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">
               Entrar na sala
